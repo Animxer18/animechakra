dd8cf0a28ffecbd111af1fc7e1fce472c160f3fe
Animxer18 <138975524+Animxer18@users.noreply.github.com>
2024-01-13 02:14:09 +0800

    Update searchAnimeAnilist.js

--- utils/searchAnimeAnilist.js
+++ utils/searchAnimeAnilist.js
@@ -3,7 +3,7 @@ export const searchAnime = async (animeQuery) => {
     try {
       const searchData = await fetch(
         // `https://api.consumet.org/meta/anilist/advanced-search?query=${animeQuery}&sort="POPULARITY_DESC"`
-        `https://animxer-api-phi.vercel.app/meta/anilist/advanced-search?query=${animeQuery}&sort="POPULARITY_DESC"`
+        `https://animxer-api-cvxg.vercel.app//meta/anilist/advanced-search?query=${animeQuery}&sort="POPULARITY_DESC"`
       );
 
       if (searchData.ok) {
